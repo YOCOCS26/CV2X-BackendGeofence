0f443a9cc7f1ec88ff2e319297baf8c657c3d189
john.nicolas.maylas.saco-ext@continental-corporation.com <uib11290@contiwan.com>
2020-02-21 17:34:45 +0800

    Add project files.

---
+++ CV2X-BackendGeofence/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV2X_BackendGeofence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
